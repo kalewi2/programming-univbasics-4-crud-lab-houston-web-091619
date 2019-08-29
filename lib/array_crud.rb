8b05a18d885d3995119214547d4d4e9573490a75
Kim Lewis <kalewi2@gmail.com>
2019-08-29 15:01:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Pinky", "Oscar", "Edgar", "Philly"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.unshift()
 end
 
 def add_element_to_start_of_array(array, element)
